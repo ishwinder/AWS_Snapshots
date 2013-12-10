604cd1fb02e6a86a5bd4395a7d929f830bb6a6c2
Enbake <enbake@Enbake-TP.(none)>
2013-12-10 11:35:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AWSSnapshots::Application.config.secret_key_base = 'ed36b65b7a647e420f52706b90b2642df819f1f9e2660e98a49ec2c4ac3b36cd41d200cea13fd01c939a8f078681bbe05a4d1a4c92cb833c68b5da5de49a87bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AWSSnapshots::Application.config.session_store :cookie_store, key: '_AWS_Snapshots_session'

8d8d84581ba28884ba69ed5ee88966d6b66ed9bb
mukul <mugoyal@enbake.com>
2013-12-11 14:32:17 +0530

    Setup redirect path after signup path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,6 +9,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 
+
   private :sign_up_params
   private :account_update_params
+
+  private
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
 end
